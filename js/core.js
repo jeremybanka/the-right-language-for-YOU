39a38ac160b64473717994589c211352c4451037
Jeremy Banka <jeremybanka@gmail.com>
2021-03-26 13:07:03 -0700

    💄 finish styling with demo content

--- js/core.js
+++ js/core.js
@@ -1,3 +1,55 @@
+const QUIZ = [
+  {
+    question: `How many satoshis do you own?`,
+    answers: [
+      {
+        text: `What's a "satoshi"?`,
+        reply: ``,
+      },
+      {
+        text: `Just a few, probably less than 100.`,
+        reply: ``,
+      },
+      {
+        text: `Millions and millions of them.`,
+        reply: ``,
+      },
+    ],
+  },
+  {
+    question: `Why do you hate Facebook so much?`,
+    answers: [
+      {
+        text: `It's so obnoxious how they always advertise at you.`,
+        reply: `Totally!`,
+      },
+      {
+        text: `They want to get me hooked and strung out!`,
+        reply: `Dopamine dealers...`,
+      },
+      {
+        text:
+          `
+            I don't like the idea that they're selling my data 
+            without asking me.
+          `,
+        reply: `Right! Or paying?!`,
+      },
+      {
+        text: `Uh, it's just a website. Why would I feel that way?`,
+        reply: `'Zuckerberg is a spooky homunculus and shouldn't be trusted.'`,
+      },
+      {
+        text:
+          `
+            Zuckerberg is a spooky homunculus who sends shivers down my spine.
+          `,
+        reply: ``,
+      },
+    ],
+  },
+]
+
 $(() => {
 
 })
