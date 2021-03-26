dd5dbdc49f8655de09526ce5a882ebc5dd776805
Jeremy Banka <jeremybanka@gmail.com>
2021-03-26 15:12:53 -0700

    ✨ flip to next page of questions

--- js/core.js
+++ js/core.js
@@ -1,7 +1,9 @@
-const QUIZ = [
+const QUIZ_PAGES = [
   {
+    id: `crypto`,
     question: `How many satoshis do you own?`,
-    answers: [
+    answer: null,
+    possibleAnswers: [
       {
         text: `What's a "satoshi"?`,
         reply: ``,
@@ -17,14 +19,19 @@ const QUIZ = [
     ],
   },
   {
+    id: `fb`,
     question: `Why do you hate Facebook so much?`,
-    answers: [
+    answer: null,
+    possibleAnswers: [
       {
         text: `It's so obnoxious how they always advertise at you.`,
         reply: `Totally!`,
       },
       {
-        text: `They want to get me hooked and strung out!`,
+        text: `
+          They want to get me hooked on delicious 
+          dopamine doses--and strung out!
+        `,
         reply: `Dopamine dealers...`,
       },
       {
@@ -36,7 +43,12 @@ const QUIZ = [
         reply: `Right! Or paying?!`,
       },
       {
-        text: `Uh, it's just a website. Why would I feel that way?`,
+        text:
+          `
+            I don't hate Facebook! 
+            It's just a website. 
+            Why would I feel that way?
+          `,
         reply: `'Zuckerberg is a spooky homunculus and shouldn't be trusted.'`,
       },
       {
@@ -50,6 +62,48 @@ const QUIZ = [
   },
 ]
 
-$(() => {
+function cleanUp() {
+  $(`main`).empty()
+}
 
+function printPage(page) {
+  const { id, question, answer, possibleAnswers } = page
+  const $question = $(`<ul id='question'><li>${question}</li></ul>`)
+  const $responseForm = $(`<form id='response'></form>`)
+  for(let idx = 0; idx < possibleAnswers.length; idx++) {
+    const possibleAnswer = possibleAnswers[idx]
+    const $possibleAnswer = $(`<label/>`)
+    const $radioButton = $(`<input type='radio'>`)
+    $radioButton.attr(`name`, id).attr(`value`, idx)
+    $($possibleAnswer).append($radioButton)
+    $($possibleAnswer).append(`<div class='border'></div>`)
+    $($possibleAnswer).append(`<span>${possibleAnswer.text}</span>`)
+    $($responseForm).append($possibleAnswer)
+  }
+  $(`main`).append($question).append($responseForm)
+}
+
+function getNextPage(pagesUnanswered, pagesAnswered, currentPage) {
+  if(pagesUnanswered.length === 0) return
+  if(currentPage) pagesAnswered.push(currentPage)
+  return pagesUnanswered.pop()
+}
+
+$(() => {
+  const pagesUnanswered = QUIZ_PAGES
+  const pagesAnswered = []
+  let currentPage
+  currentPage = getNextPage(pagesUnanswered, pagesAnswered, currentPage)
+  printPage(currentPage)
+  $(`button[type='submit']`).on(`click`, e => {
+    e.preventDefault()
+    const currentPageMemo = currentPage
+    currentPage = getNextPage(pagesUnanswered, pagesAnswered, currentPage)
+    if(currentPage) {
+      cleanUp()
+      printPage(currentPage)
+    } else {
+      currentPage = currentPageMemo
+    }
+  })
 })
