66e9eb7e8e74ad44479f709ae3a8fef02d97da64
Jeremy Banka <jeremybanka@gmail.com>
2021-03-26 18:03:59 -0700

    🚧 score page in progress. need a break

--- js/core.js
+++ js/core.js
@@ -1,88 +1,144 @@
+const LANGUAGE_NAMES = {
+  js: `Javascript`,
+  ruby: `Ruby`,
+  hoon: `Hoon`,
+}
 const QUIZ_PAGES = [
   {
-    id: `crypto`,
-    question: `How many satoshis do you own?`,
-    answer: null,
+    id: `build`,
+    question: `What do you want to build?`,
     possibleAnswers: [
       {
-        text: `What's a "satoshi"?`,
-        reply: ``,
+        text:
+          `
+          I want to build applications that process complex data 
+          behind the scenes, giving the user powerful data and insights.
+          `,
+        scores: { js: 0, ruby: 2, hoon: 0 },
       },
       {
-        text: `Just a few, probably less than 100.`,
-        reply: ``,
+        text:
+          `
+          I want to make only the tastiest user interfaces and workflows.
+          The DOM is my DOMain.
+          `,
+        scores: { js: 2, ruby: 0, hoon: 0 },
       },
       {
-        text: `Millions and millions of them.`,
-        reply: ``,
+        text:
+          `
+          I want to build alien technology.
+          `,
+        scores: { js: 0, ruby: 1, hoon: 2 },
       },
     ],
   },
   {
-    id: `fb`,
-    question: `Why do you hate Facebook so much?`,
-    answer: null,
+    id: `verbosity`,
+    question: `Do like words or symbols?`,
     possibleAnswers: [
       {
-        text: `It's so obnoxious how they always advertise at you.`,
-        reply: `Totally!`,
-      },
-      {
-        text: `
-          They want to get me hooked on delicious 
-          dopamine doses--and strung out!
-        `,
-        reply: `Dopamine dealers...`,
+        text:
+          `
+          Words! Normal words are the best, even if they are long. 
+          I hate having to remember weird shorthands for things,
+          odd terminology, or arcane symbols.
+          `,
+        scores: { js: 2, ruby: 1, hoon: 0 },
       },
       {
         text:
           `
-            I don't like the idea that they're selling my data 
-            without asking me.
+          I like words, but short words are best.
           `,
-        reply: `Right! Or paying?!`,
+        scores: { js: 1, ruby: 2, hoon: 0 },
       },
       {
         text:
           `
-            I don't hate Facebook! 
-            It's just a website. 
-            Why would I feel that way?
+          I like weird words and strange symbols. 
+          They make me feel like a wizard.
           `,
-        reply: `'Zuckerberg is a spooky homunculus and shouldn't be trusted.'`,
+        scores: { js: 0, ruby: 1, hoon: 2 },
+      },
+    ],
+  },
+  {
+    id: `paradigm`,
+    question: `Do you prefer declarative or imperative programming?`,
+    possibleAnswers: [
+      {
+        text:
+          `
+          Declarative (functional languages are declarative.)
+          `,
+        scores: { js: 0, ruby: 2, hoon: 2 },
       },
       {
         text:
           `
-            Zuckerberg is a spooky homunculus who sends shivers down my spine.
+          Imperative (object-oriented languages are imperative.)
           `,
-        reply: ``,
+        scores: { js: 2, ruby: 2, hoon: 0 },
+      },
+    ],
+  },
+  {
+    id: `typing`,
+    question: `Do you prefer static or dynamic typing?`,
+    possibleAnswers: [
+      {
+        text:
+          `
+          Static
+          `,
+        scores: { js: 0, ruby: 0, hoon: 2 },
+      },
+      {
+        text:
+          `
+          Dynamic
+          `,
+        scores: { js: 2, ruby: 2, hoon: 0 },
       },
     ],
   },
 ]
 
-function cleanUp() {
-  $(`main`).empty()
-}
-
-function printPage(page) {
-  const { id, question, answer, possibleAnswers } = page
-  const $question = $(`<ul id='question'><li>${question}</li></ul>`)
-  const $responseForm = $(`<form id='response'></form>`)
-  for(let idx = 0; idx < possibleAnswers.length; idx++) {
-    const possibleAnswer = possibleAnswers[idx]
-    const $possibleAnswer = $(`<label/>`)
-    const $radioButton = $(`<input type='radio'>`)
-    $radioButton.attr(`name`, id).attr(`value`, idx)
-    $($possibleAnswer).append($radioButton)
-    $($possibleAnswer).append(`<div class='border'></div>`)
-    $($possibleAnswer).append(`<span>${possibleAnswer.text}</span>`)
-    $($responseForm).append($possibleAnswer)
+function determineWinner(scoreSheet) {
+  let indexOfWinner = 0
+  let highScore = 0
+  for(let idx = 0; idx < scoreSheet.length; idx++) {
+    const languageResult = scoreSheet[idx]
+    if(languageResult.score > highScore) {
+      indexOfWinner = idx
+      highScore = languageResult.score
+    }
   }
-  $(`main`).append($question).append($responseForm)
+  const idOfWinner = scoreSheet[indexOfWinner].id
+  const winningLanguage = LANGUAGE_NAMES[idOfWinner]
+  return winningLanguage
 }
-
+function scoreQuiz(pagesAnswered) {
+  let jsTotal = 0
+  let rubyTotal = 0
+  let hoonTotal = 0
+  for(let idx = 0; idx < pagesAnswered.length; idx++) {
+    const page = pagesAnswered[idx]
+    if(typeof page.answerIdx === `undefined`) return
+    const chosenAnswer = page.possibleAnswers[page.answerIdx]
+    const { js, ruby, hoon } = chosenAnswer.scores
+    jsTotal += js
+    rubyTotal += ruby
+    hoonTotal += hoon
+  }
+  const scoreSheet = [
+    { id: `js`, score: jsTotal },
+    { id: `ruby`, score: rubyTotal },
+    { id: `hoon`, score: hoonTotal },
+  ]
+  return scoreSheet
+}
 function getNextPage(pagesUnanswered, pagesAnswered, currentPage) {
   if(pagesUnanswered.length === 0) return
   if(currentPage) pagesAnswered.push(currentPage)
@@ -94,32 +150,87 @@ function getPrevPage(pagesUnanswered, pagesAnswered, currentPage) {
   return pagesAnswered.pop()
 }
 
+function sendMessage(string) {
+  const $message = $(`#message`)
+  $message.removeClass().show()
+  $(`#message`).text(string).addClass(`fade-out`)
+}
+function print($content) {
+  $(`main`).append($content)
+}
+function purge() {
+  $(`main`).empty()
+}
+function $RadioButton({ id, idx }) {
+  const $radio = $(`<input type='radio'>`)
+  $radio.attr(`name`, id).attr(`value`, idx)
+  return $radio
+}
+function $PossibleAnswer({ possibleAnswer, idx, id }) {
+  const { text } = possibleAnswer
+  const $label = $(`<label/>`)
+  const $border = $(`<div class='border'/>`)
+  const $textSpan = $(`<span/>`).text(text)
+  return (
+    $label.append(
+      $RadioButton({ id, idx }),
+      $border,
+      $textSpan
+    )
+  )
+}
+function $QuizPage(quizPage) {
+  const { id, question, possibleAnswers } = quizPage
+  const $question = $(`<ul id='question'><li>${question}</li></ul>`)
+  const $responseForm = $(`<form id='response'/>`)
+  for(let idx = 0; idx < possibleAnswers.length; idx++) {
+    const possibleAnswer = possibleAnswers[idx]
+    $responseForm.append($PossibleAnswer({ possibleAnswer, idx, id }))
+  }
+  return [$question, $responseForm]
+}
+function $ScorePage(winningLanguage) {
+  const content = `${winningLanguage} was your most compatible language!`
+  const $article = $(`<article/>`).text(content)
+  const $scorePage = $article.addClass(`score`)
+  return $scorePage
+}
 $(() => {
   const pagesUnanswered = QUIZ_PAGES
   const pagesAnswered = []
   let currentPage
   currentPage = getNextPage(pagesUnanswered, pagesAnswered, currentPage)
-  printPage(currentPage)
+  print($QuizPage(currentPage))
   $(`button[type='submit']`).on(`click`, e => {
     e.preventDefault()
     const currentPageMemo = currentPage
     currentPage = getNextPage(pagesUnanswered, pagesAnswered, currentPage)
     if(currentPage) {
-      cleanUp()
-      printPage(currentPage)
+      purge()
+      print($QuizPage(currentPage))
     } else {
-      currentPage = currentPageMemo
+      const scoreSheet = scoreQuiz(pagesAnswered.push(currentPage))
+      const winningLanguage = determineWinner(scoreSheet)
+      console.log(scoreSheet, winningLanguage)
+      if(winningLanguage) {
+        purge()
+        $(`footer`).hide()
+        print($ScorePage(winningLanguage))
+      } else {
+        sendMessage(`Finish the quiz before submitting!`)
+        currentPage = currentPageMemo
+      }
     }
   })
   $(`button[type='button']`).on(`click`, () => {
     const currentPageMemo = currentPage
     currentPage = getPrevPage(pagesUnanswered, pagesAnswered, currentPage)
     if(currentPage) {
-      cleanUp()
-      printPage(currentPage)
+      purge()
+      print($QuizPage(currentPage))
     } else {
       currentPage = currentPageMemo
     }
   })
-  $(`[type='radio']`).on(`click`, e => currentPage.answer = e.target.value)
+  $(`[type='radio']`).on(`click`, e => currentPage.answerIdx = e.target.value)
 })
